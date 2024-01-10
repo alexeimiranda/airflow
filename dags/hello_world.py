1c33e07a194345a1c9e36b5e4fda62cac2167d78
Alexei Miranda <alexei.miranda@inter.co>
2024-01-10 18:17:27 -0300

    1.0.0

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -12,12 +12,12 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('hello_world_dag',
+dag = DAG('hello_world',
           default_args=default_args,
-          description='A simple Hello World DAG1',
+          description='Hello World DAG',
           schedule_interval='0 12 * * *',  # Run once a day at noon
           catchup=False)
 
-hello_world_task = PythonOperator(task_id='hello_world_task1',
+hello_world_task = PythonOperator(task_id='hello_world_task',
                                   python_callable=hello_world,
                                   dag=dag)
